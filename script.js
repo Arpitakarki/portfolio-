8090e6b01b9d0badee34adc98347e669a75e3a58
Arpita karki <151529261+Arpitakarki@users.noreply.github.com>
2024-04-12 10:20:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+setTimeout( showform, 1000);
+
+function showform() {
+    document.getElementById('showform').style.display = "block";
+}
+function hideform() {
+    document.getElementById('showform').style.display = "none";
+}
